bc72b563fb3ae8f48df2890ac6ec0b4fd9e58165
ischeini <ischeini@Ism4GOL>
2025-07-27 17:03:22 +0200

    refractor: changed token and commands arrays to lists

--- src/parser.c
+++ src/parser.c
@@ -3,36 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sscheini <sscheini@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ischeini <ischeini@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 17:56:26 by sscheini          #+#    #+#             */
-/*   Updated: 2025/07/25 19:07:52 by sscheini         ###   ########.fr       */
+/*   Updated: 2025/07/27 16:41:11 by ischeini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "parser.h"
 
-t_token	*tokenize(t_body *minishell, char **split)
+
+static int	verify_tokens(t_body *minishell, t_token *arr)
 {
-	t_token	*arr;
-	t_token	*new;
+	
+}
+
+static t_token	*tokenize(t_body *minishell, char **split)
+{
+	t_list	**token_lst;
+	t_list	*new_node;
+	t_token	*new_token;
 	int		i;
 
 	i = -1;
-	arr = NULL;
+	tokens = NULL;
 	while(split[++i])
 	{
-		new = new_token(minishell, split[i]);
+		new_token = create_token(minishell, split[i]);
+		new = ft_lstnew(new_token);
 		if (!new)
-			return (sigend(minishell, 1));
-		token_add_back(&arr, new);
+			sigend(minishell, 1);
+		add_back_token(&arr, new);
 	}
 	free(split);
 	return (arr);
 }
 
-void	parser(t_body *minishell, char *input)
+t_cmd	*parser(t_body *minishell, char *input)
 {
 	t_token	*arr;
 	char	**split;
@@ -40,5 +48,10 @@ void	parser(t_body *minishell, char *input)
 	split = ft_iq_split(input, ' ');
 	if (!split)
 		sigend(minishell, 1);
+	free(input);
 	arr = tokenize(minishell, split);
+	if (!arr)
+		sigend(minishell, 2);
+	if (verify_tokens(minishell, arr))
+		sigend(minishell, 2);
 }
--- src/parser_utils.c
+++ src/parser_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sscheini <sscheini@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ischeini <ischeini@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 18:41:14 by sscheini          #+#    #+#             */
-/*   Updated: 2025/07/25 19:05:52 by sscheini         ###   ########.fr       */
+/*   Updated: 2025/07/27 16:39:34 by ischeini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,56 +30,20 @@ static int	get_token_type(char *str)
 }
 
 /**
- * Searches for a pointer to the last LIST node to be found.
- * @param lst The starting node.
- * @return A pointer to the last LIST node.
- * @note If the starting node doesn't exists, returns NULL.
- */
-t_token	*last_token(t_token *lst)
-{
-	if (!lst)
-		return (NULL);
-	while (lst->next != NULL)
-		lst = lst->next;
-	return (lst);
-}
-
-/**
- * Creates and allocates a new LIST node.
- * @param content A pointer to the content to be included inside of the new
- * node.
+ * Creates and allocates a new T_TOKEN node.
+ * @param minishell A pointer to the main enviroment structure of minishell.
+ * @param content A pointer to the STRING to be tokenized.
  * @return A pointer to the new LIST node; or NULL in case of error.
  * @note The next node inside of the LIST node is set to NULL.
  */
-t_token	*new_token(t_body *minishell, char	*content)
+t_token	*create_token(t_body *minishell, char *str)
 {
 	t_token	*new;
 
 	new = malloc(sizeof(t_token));
 	if (!new)
-		return (sigend(minishell, 1));
-	new->str = content;
-	new->type = get_token_type(content);
-	new->next = NULL;
+		sigend(minishell, 1);
+	new->str = str;
+	new->type = get_token_type(str);
 	return (new);
-}
-
-/**
- * Adds a lst node at the end of a lst HEAD.
- * @param lst The LIST HEAD where to add the new node.
- * @param new The new LIST node to add.
- * @note If the LIST HEAD is empty, sets the new LIST node at the start
- * of it.
- */
-void	add_back_token(t_token **lst, t_token *new)
-{
-	t_token	*tmp;
-
-	tmp = last_token(*lst);
-	if (!tmp)
-	{
-		*lst = new;
-		return ;
-	}
-	tmp->next = new;
 }
